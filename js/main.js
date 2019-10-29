65c0cc0e05d6ec8f9b7c4623885a1e865b07f417
Anton <aseyev.1982@gmail.com>
2019-10-29 11:28:05 +0700

    upload

---
+++ js/main.js
@@ -0,0 +1,81 @@
+const form = $('#price-form');
+
+let formData = form.serializeJSON();
+
+function showHideBlocks() {
+    if ( formData.type == 'site') {
+        $('[data-name="pages"]').show();
+        $('[data-name="landing"]').hide();
+        $('[name="sections"]').val('0');
+    } else {
+        $('[data-name="pages"]').hide();
+        $('[data-name="landing"]').show();
+        $('[name="pages-unique"]').val('0');
+        $('[name="pages-general"]').val('0');
+    }
+    if ( formData.mobile == 'on') {
+        $('[data-name="mobile"]').show();
+    } else {
+        $('[data-name="mobile"]').hide();
+        $('[name="mobile-number"]')[0].checked = true;
+        // $('[name="mobile-number"]')[1].checked = false; зачем менять ВСЕ радиобаттоны? Активен то 1.
+        // $('[name="mobile-number"]')[2].checked = false;
+    }
+}
+//отобразили нужные блоки при загрузке
+showHideBlocks();
+
+// отслеживаем клики-кнопки в форме
+form.on ('keyup change paste', 'input, select, textarea', function(){
+
+    // отслеживаем и меняем блоки в формах, удаляем инфу из скрытых блоков 
+    formData = form.serializeJSON();
+    showHideBlocks();
+    // console.log('1 ', formData);
+
+    // обновляем данные в форме для расчета цены
+    formData = form.serializeJSON();
+
+    updatePrice( calculatePrice() );
+});
+
+function calculatePrice() {
+    // начинаем считать цену
+    let totalPrice = 0;
+    totalPrice = 
+        formData['pages-unique'] * 4000 + 
+        formData['pages-general'] * 2500 +
+        formData['sections'] * 2000 +
+        formData['carousel'] * 1200 +
+        formData['modals'] * 900 +
+        formData['forms'] * 1500;
+
+    // мобильный мультипликатор
+    let multipleMobile = 1;
+    if (formData['mobile-number'] == 2) {
+        multipleMobile = 1.3;
+    } else if (formData['mobile-number'] == 3) {
+        multipleMobile = 1.5;
+    }
+
+    // дополнительные мультипликаторы
+    let mPixelPerfect = 1;
+    if (formData['pixelPerfect'] == 'on') {mPixelPerfect = 1.2;}
+
+    let mRetina = 1;
+    if (formData['retinaReady'] == 'on') {mRetina = 1.2;}
+
+    let mGooglePageSpeed = 1;
+    if (formData['googlePageSpeed'] == 'on') {mGooglePageSpeed = 1.2;}
+
+    let mUrgent = 1;
+    if (formData['urgentOrder'] == 'on') {mUrgent = 1.5;}
+
+    totalPrice = totalPrice * multipleMobile * mPixelPerfect * mRetina * mGooglePageSpeed * mUrgent;
+
+    return totalPrice;
+}
+
+function updatePrice(price) {
+    $('#total-price').text(price);
+}
